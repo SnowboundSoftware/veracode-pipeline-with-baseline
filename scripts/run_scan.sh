ee0c0022f3b7c2f2e46a5feb66f2d9193692a0a5
Matt Runkle <matthew.runkle@smartbear.com>
2021-05-28 17:34:37 -0400

    Add additional param validation

--- scripts/run_scan.sh
+++ scripts/run_scan.sh
@@ -1,5 +1,19 @@
 #!/bin/sh -l
 
+# Check for required values
+if [ -z "${INPUT_VID}" ] || [ -z "${INPUT_VKEY}" ]
+then
+    echo 'The Veracode API ID and KEY must be provided to submit a pipeline scan!'
+    exit 1
+fi
+
+if [ -z "${INPUT_FILENAME}" ]
+then
+    echo 'A file must be provided to scan!'
+    exit 1
+fi
+
+# Check optional parameters, and set commands
 results=''
 if [ -n "${INPUT_RESULTS}" ]
 then
@@ -7,7 +21,7 @@ then
 fi
 
 baseline=''
-if [ -n "${INPUT_BASELINE}" ]
+if [ -n "${INPUT_BASELINE}" ] && [ -f "${INPUT_BASELINE}" ]
 then
     baseline="--baseline_file ${INPUT_BASELINE}"
 fi
@@ -44,9 +58,16 @@ java -jar pipeline-scan.jar -vid "${INPUT_VID}" -vkey "${INPUT_VKEY}" -f "${INPU
 ret=$?
 
 # Determine exit status
-if [ "${INPUT_ALLOWFAIL}" == 'false' ] || [ "${INPUT_ALLOWFAIL}" == '0' ]
+if [ ${ret} -eq -1 ]
 then
-    exit 0  # Job will always succeed, regardless of scan results
+    # We only capture -1 as that indicates a scan error. Code of -3 indicates a timeout, which we may suppress
+    echo 'An error with the pipeline scan occurred!'
+    exit -1
+elif [ "${INPUT_ALLOWFAIL}" == 'false' ] || [ "${INPUT_ALLOWFAIL}" == '0' ]
+then
+    # Job will always succeed, regardless of scan results
+    exit 0
 else
-    exit "$ret"  # Job will fail based on scan criteria and results
+    # Job will fail based on scan criteria and results
+    exit ${ret}
 fi
